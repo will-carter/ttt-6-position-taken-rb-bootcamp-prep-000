e255e854170094ef128e0b9cd66448ce4e760c66
Will Carter <willmckenna64@gmail.com>
2018-06-20 20:34:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index == "X" || index == "O"
+  if board[index] == "X" || board[index] == "O"
     return true
   else
     return false
