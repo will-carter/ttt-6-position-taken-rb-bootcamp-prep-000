4b7ee61dc750d391f85dca356bbf486b8ec01bf4
Will Carter <willmckenna64@gmail.com>
2018-06-20 20:12:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if index == "" || " "
     return false
   elsif
-    index == "X" || index == "O"
+    index.to_s == "X" || index == "O"
     return true
   end
 end
